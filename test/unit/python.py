57841fa77115cc210090c1b28f23e179ebc0fb40
Trupti  Shantaram Chaudhary <trupti.c@cloudfulcrum.com>
2023-12-06 16:58:12 +0530

    rt

---
+++ test/unit/python.py
@@ -0,0 +1,10 @@
+# my_yaml_reader.py
+import yaml
+
+class MyYAMLReader:
+    def __init__(self, file_path):
+        with open(file_path, 'r') as file:
+            self.config = yaml.safe_load(file)
+
+    def get_config_value(self, section, key):
+        return self.config[section][key]
